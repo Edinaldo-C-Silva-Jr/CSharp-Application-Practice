07b5bcb729a9fe2b439319297dbe6432e99cfea9
Edinaldo C da Silva Jr <edinaldo.csilvajr@outlook.com>
2023-07-22 16:54:19 -0300

    Made it possible to interact with the form while the simulation is running. Also added explanations for the riddle and answer.

--- Riddle Simulations/MultiverseRescueRiddle/MultiverseRescueRiddle/MainForm.Designer.cs
+++ Riddle Simulations/MultiverseRescueRiddle/MultiverseRescueRiddle/MainForm.Designer.cs
@@ -32,6 +32,7 @@ namespace MultiverseRescueRiddle
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.gpb_dimension1 = new System.Windows.Forms.GroupBox();
 			this.lbl_d1Robot = new System.Windows.Forms.Label();
 			this.lbl_d1TurnSaved = new System.Windows.Forms.Label();
@@ -107,6 +108,9 @@ namespace MultiverseRescueRiddle
 			this.cbb_delay = new System.Windows.Forms.ComboBox();
 			this.btn_start = new System.Windows.Forms.Button();
 			this.btn_riddle = new System.Windows.Forms.Button();
+			this.btn_answer = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tmr_interval = new System.Windows.Forms.Timer(this.components);
 			this.gpb_dimension1.SuspendLayout();
 			this.gpb_dimension2.SuspendLayout();
 			this.gpb_dimension4.SuspendLayout();
@@ -673,7 +677,7 @@ namespace MultiverseRescueRiddle
 			this.gpb_Robot.Controls.Add(this.lbl_leftA);
 			this.gpb_Robot.Controls.Add(this.tbr_leftLever);
 			this.gpb_Robot.Controls.Add(this.tbr_rightLever);
-			this.gpb_Robot.Location = new System.Drawing.Point(250, 10);
+			this.gpb_Robot.Location = new System.Drawing.Point(230, 10);
 			this.gpb_Robot.Name = "gpb_Robot";
 			this.gpb_Robot.Size = new System.Drawing.Size(120, 120);
 			this.gpb_Robot.TabIndex = 21;
@@ -755,7 +759,7 @@ namespace MultiverseRescueRiddle
 			this.gpb_stats.Controls.Add(this.lbl_peopleCounted);
 			this.gpb_stats.Controls.Add(this.lbl_peopleSaved);
 			this.gpb_stats.Controls.Add(this.lbl_turns);
-			this.gpb_stats.Location = new System.Drawing.Point(390, 10);
+			this.gpb_stats.Location = new System.Drawing.Point(370, 10);
 			this.gpb_stats.Name = "gpb_stats";
 			this.gpb_stats.Size = new System.Drawing.Size(150, 120);
 			this.gpb_stats.TabIndex = 22;
@@ -794,16 +798,16 @@ namespace MultiverseRescueRiddle
 			this.gpb_settings.Controls.Add(this.chk_pauseSave);
 			this.gpb_settings.Controls.Add(this.cbb_delay);
 			this.gpb_settings.Controls.Add(this.btn_start);
-			this.gpb_settings.Location = new System.Drawing.Point(560, 10);
+			this.gpb_settings.Location = new System.Drawing.Point(540, 10);
 			this.gpb_settings.Name = "gpb_settings";
-			this.gpb_settings.Size = new System.Drawing.Size(250, 120);
+			this.gpb_settings.Size = new System.Drawing.Size(230, 120);
 			this.gpb_settings.TabIndex = 23;
 			this.gpb_settings.TabStop = false;
 			this.gpb_settings.Text = "Settings";
 			// 
 			// btn_reset
 			// 
-			this.btn_reset.Location = new System.Drawing.Point(130, 20);
+			this.btn_reset.Location = new System.Drawing.Point(120, 20);
 			this.btn_reset.Name = "btn_reset";
 			this.btn_reset.Size = new System.Drawing.Size(95, 25);
 			this.btn_reset.TabIndex = 5;
@@ -813,7 +817,7 @@ namespace MultiverseRescueRiddle
 			// 
 			// lbl_interval
 			// 
-			this.lbl_interval.Location = new System.Drawing.Point(50, 58);
+			this.lbl_interval.Location = new System.Drawing.Point(40, 58);
 			this.lbl_interval.Name = "lbl_interval";
 			this.lbl_interval.Size = new System.Drawing.Size(70, 15);
 			this.lbl_interval.TabIndex = 4;
@@ -821,7 +825,7 @@ namespace MultiverseRescueRiddle
 			// 
 			// chk_pauseCount
 			// 
-			this.chk_pauseCount.Location = new System.Drawing.Point(130, 85);
+			this.chk_pauseCount.Location = new System.Drawing.Point(120, 85);
 			this.chk_pauseCount.Name = "chk_pauseCount";
 			this.chk_pauseCount.Size = new System.Drawing.Size(104, 24);
 			this.chk_pauseCount.TabIndex = 3;
@@ -830,7 +834,7 @@ namespace MultiverseRescueRiddle
 			// 
 			// chk_pauseSave
 			// 
-			this.chk_pauseSave.Location = new System.Drawing.Point(20, 85);
+			this.chk_pauseSave.Location = new System.Drawing.Point(10, 85);
 			this.chk_pauseSave.Name = "chk_pauseSave";
 			this.chk_pauseSave.Size = new System.Drawing.Size(104, 24);
 			this.chk_pauseSave.TabIndex = 2;
@@ -848,14 +852,15 @@ namespace MultiverseRescueRiddle
 									"0,5",
 									"0,75",
 									"1"});
-			this.cbb_delay.Location = new System.Drawing.Point(120, 55);
+			this.cbb_delay.Location = new System.Drawing.Point(110, 55);
 			this.cbb_delay.Name = "cbb_delay";
 			this.cbb_delay.Size = new System.Drawing.Size(80, 21);
 			this.cbb_delay.TabIndex = 1;
+			this.cbb_delay.SelectedIndexChanged += new System.EventHandler(this.Cbb_delaySelectedIndexChanged);
 			// 
 			// btn_start
 			// 
-			this.btn_start.Location = new System.Drawing.Point(25, 20);
+			this.btn_start.Location = new System.Drawing.Point(15, 20);
 			this.btn_start.Name = "btn_start";
 			this.btn_start.Size = new System.Drawing.Size(95, 25);
 			this.btn_start.TabIndex = 0;
@@ -865,18 +870,44 @@ namespace MultiverseRescueRiddle
 			// 
 			// btn_riddle
 			// 
-			this.btn_riddle.Location = new System.Drawing.Point(816, 12);
+			this.btn_riddle.Location = new System.Drawing.Point(780, 30);
 			this.btn_riddle.Name = "btn_riddle";
-			this.btn_riddle.Size = new System.Drawing.Size(30, 30);
+			this.btn_riddle.Size = new System.Drawing.Size(60, 30);
 			this.btn_riddle.TabIndex = 24;
-			this.btn_riddle.Text = "?";
+			this.btn_riddle.Text = "Riddle";
 			this.btn_riddle.UseVisualStyleBackColor = true;
+			this.btn_riddle.Click += new System.EventHandler(this.Btn_riddleClick);
+			// 
+			// btn_answer
+			// 
+			this.btn_answer.Location = new System.Drawing.Point(780, 65);
+			this.btn_answer.Name = "btn_answer";
+			this.btn_answer.Size = new System.Drawing.Size(60, 30);
+			this.btn_answer.TabIndex = 25;
+			this.btn_answer.Text = "Answer";
+			this.btn_answer.UseVisualStyleBackColor = true;
+			this.btn_answer.Click += new System.EventHandler(this.Btn_answerClick);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(775, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(70, 20);
+			this.label1.TabIndex = 26;
+			this.label1.Text = "Explanations";
+			// 
+			// tmr_interval
+			// 
+			this.tmr_interval.Interval = 1;
+			this.tmr_interval.Tick += new System.EventHandler(this.Tmr_intervalTick);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(859, 412);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btn_answer);
 			this.Controls.Add(this.btn_riddle);
 			this.Controls.Add(this.gpb_settings);
 			this.Controls.Add(this.gpb_stats);
@@ -914,6 +945,9 @@ namespace MultiverseRescueRiddle
 			this.gpb_settings.ResumeLayout(false);
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Timer tmr_interval;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btn_answer;
 		private System.Windows.Forms.Button btn_reset;
 		private System.Windows.Forms.Button btn_riddle;
 		private System.Windows.Forms.Button btn_start;
--- Riddle Simulations/MultiverseRescueRiddle/MultiverseRescueRiddle/MainForm.cs
+++ Riddle Simulations/MultiverseRescueRiddle/MultiverseRescueRiddle/MainForm.cs
@@ -19,6 +19,7 @@ namespace MultiverseRescueRiddle
 		private int turnsTaken = 0, peopleSaved = 0, peopleCounted = 0, robotPosition = 0;
 		
 		private bool rightLever = false, leftLever = false, buttonPressed = false;
+		private string groupBoxName = "", labelName = "";
 		Random robotTeleport;
 		
 		public MainForm()
@@ -31,6 +32,8 @@ namespace MultiverseRescueRiddle
 			robotTeleport = new Random();
 			
 			cbb_delay.SelectedIndex = 2;
+			delay = 1000 * float.Parse(cbb_delay.SelectedItem.ToString());
+			tmr_interval.Interval = (int)delay;
 		}
 		
 		private void Btn_startClick(object sender, EventArgs e)
@@ -41,92 +44,7 @@ namespace MultiverseRescueRiddle
 				return;
 			}
 			
-			string groupBoxName = "", labelName = "";
-			delay = 1000 * float.Parse(cbb_delay.SelectedItem.ToString());
-			
-			do
-			{
-				if (turnsTaken > 0)
-				{
-					labelName = "lbl_d" + robotPosition + "Robot";
-					this.Controls[groupBoxName].Controls[labelName].Visible = false;
-				}
-				
-				turnsTaken++;
-				robotPosition = robotTeleport.Next(1, 12);
-				timesVisited[robotPosition - 1]++;
-				groupBoxName = "gpb_dimension" + robotPosition;
-				
-				if (robotPosition == 1)
-				{
-					if (!personIsSaved[robotPosition - 1])
-					{
-						personIsSaved[robotPosition - 1] = true;
-						peopleSaved++;
-						
-						labelName = "lbl_d" + robotPosition + "Saved";
-						this.Controls[groupBoxName].Controls[labelName].Text = "Saved: Yes";
-						this.Controls[groupBoxName].Controls[labelName].ForeColor = Color.Green;
-						
-						labelName = "lbl_d" + robotPosition + "TurnSaved";
-						this.Controls[groupBoxName].Controls[labelName].Text = "On Turn: " + turnsTaken;
-					}
-					
-					if (leftLever)
-					{
-						leftLever = false;
-						peopleCounted++;
-					}
-					else
-					{
-						rightLever = !rightLever;
-					}
-					
-					if (peopleCounted == 10)
-					{
-						buttonPressed = true;
-						lbl_buttonFinish.Text = "Home: Yes!";
-						lbl_buttonFinish.ForeColor = Color.Green;
-					}
-				}
-				else
-				{
-					if (!leftLever && !personIsSaved[robotPosition - 1])
-					{
-						personIsSaved[robotPosition - 1] = true;
-						peopleSaved++;
-						leftLever = true;
-						
-						labelName = "lbl_d" + robotPosition + "Saved";
-						this.Controls[groupBoxName].Controls[labelName].Text = "Saved: Yes";
-						this.Controls[groupBoxName].Controls[labelName].ForeColor = Color.Green;
-						
-						labelName = "lbl_d" + robotPosition + "TurnSaved";
-						this.Controls[groupBoxName].Controls[labelName].Text = "On Turn: " + turnsTaken;
-					}
-					else
-					{
-						rightLever = !rightLever;
-					}
-				}
-				
-				tbr_leftLever.Value = Convert.ToInt32(leftLever);
-				tbr_rightLever.Value = Convert.ToInt32(rightLever);
-				
-				lbl_peopleSaved.Text = "People Saved: " + peopleSaved.ToString();
-				lbl_peopleCounted.Text = "People Counted: " + peopleCounted.ToString();
-				lbl_turns.Text = "Turns Taken: " + turnsTaken.ToString();
-				
-				labelName = "lbl_d" + robotPosition + "TimesVisited";
-				this.Controls[groupBoxName].Controls[labelName].Text = "Times Visited: " + timesVisited[robotPosition - 1];
-				
-				labelName = "lbl_d" + robotPosition + "Robot";
-				this.Controls[groupBoxName].Controls[labelName].Visible = true;
-				
-				this.Refresh();
-				Thread.Sleep((int)delay);
-			}
-			while(!buttonPressed);
+			tmr_interval.Enabled = true;
 		}
 		
 		void Btn_resetClick(object sender, EventArgs e)
@@ -163,6 +81,128 @@ namespace MultiverseRescueRiddle
 				
 				labelName = "lbl_d" + i + "Robot";
 				this.Controls[groupBoxName].Controls[labelName].Visible = false;
+			}
+		}
+		
+		void Btn_riddleClick(object sender, EventArgs e)
+		{
+			string explanation;
+			
+			explanation = "Welcome! \nThis is a simulation for the solution of a riddle known as 'The Multiverse Rescue Riddle.'"
+				+ "This riddle can be found on the Youtube channel Ted-ED. \n\nA basic description of it is: \n"
+				+ "You and 10 other people are stuck in 11 different dimensions. Your only chance of going back home is being rescued by the Multiverse Robot. \n\n"
+				+ "The robot has 2 levers, which can be set to either position A or B, and a button. The way it works is by randomly teleporting between any of the 11 dimensions. Once it appears, it will stay there until one of its levers is flipped, which lever and which position does not matter. Once a person flips a lever, the robot knows their dimensional position, and will be able to teleport them back. Once the button is pressed, everyone who has previously touched a lever gets teleported back home. The ones who didn't touch a lever will remain stuck. \n\n"
+				+ "The robot keeps teleporting, randomly, and always requires one of its levers to be flipped before it can teleport again. So if a person who has already been visited sees the robot again, they'll have to flip the lever, again. \n\n"
+				+ "The goal of the riddle is to find a strategy that guarantees everyone will be saved. The people trapped can only communicate with each other, to figure out the plan, before the robot is activated. Once it is active, the only communication is the position of the levers. \n\n"
+				+ "Can you figure out a strategy?";
+			MessageBox.Show(explanation, "The Riddle", MessageBoxButtons.OK);
+		}
+		
+		void Btn_answerClick(object sender, EventArgs e)
+		{
+			string explanation;
+			
+			explanation = "This is the solution for the Multiverse Rescue Riddle, that the program is set to simulate. \n\n"
+				+ "Since there are only 2 levers, they are not enough to communicate who has been visited by the robot. And it wouldn't be a certain result because people are required to flip a lever, even if they have already been saved. \n\n"
+				+ "The solution then relies on the fact not everyone needs to know how many people were visited. If only one person becomes responsible for pressing the button, only that person will need to know when everyone has been visited. The person doesn't even need to know who has been visited, they only need to make sure the robot visited 11 people in the end (including themselves). \n\n"
+				+ "Once this person (let's call them 'leader') has been defined, the plan is the following: The left lever will be used to count visits, and the right lever to just send the robot on its way. At the start, the lever starts down. When someone is visited by the robot for the first time, they should flip the left lever up, to signal they're a newly saved person. After this, only the leader can pull it down, to signal they acknowledge there is a newly saved person. If anyone, other than the leader, finds the left lever already up, they should not touch it, even if they haven't been visited yet. Also, after a person has flipped the left lever up, they're officially saved, and should only use the right lever until the end of the simulation. \n\n"
+				+ "For the sake of this simulation, the leader is always the person in dimension 1. So everytime someone is saved, the robot needs to visit dimension 1 before saving a new person. After 10 people have been counted (the leader doesn't need to count themselves), the simulation ends.";
+			MessageBox.Show(explanation, "The Answer", MessageBoxButtons.OK);
+		}
+		
+		void Tmr_intervalTick(object sender, EventArgs e)
+		{
+			if (turnsTaken > 0)
+			{
+				labelName = "lbl_d" + robotPosition + "Robot";
+				this.Controls[groupBoxName].Controls[labelName].Visible = false;
+			}
+			
+			turnsTaken++;
+			robotPosition = robotTeleport.Next(1, 12);
+			timesVisited[robotPosition - 1]++;
+			groupBoxName = "gpb_dimension" + robotPosition;
+			
+			if (robotPosition == 1)
+			{
+				if (!personIsSaved[robotPosition - 1])
+				{
+					personIsSaved[robotPosition - 1] = true;
+					peopleSaved++;
+					
+					labelName = "lbl_d" + robotPosition + "Saved";
+					this.Controls[groupBoxName].Controls[labelName].Text = "Saved: Yes";
+					this.Controls[groupBoxName].Controls[labelName].ForeColor = Color.Green;
+					
+					labelName = "lbl_d" + robotPosition + "TurnSaved";
+					this.Controls[groupBoxName].Controls[labelName].Text = "On Turn: " + turnsTaken;
+				}
+				
+				if (leftLever)
+				{
+					leftLever = false;
+					peopleCounted++;
+				}
+				else
+				{
+					rightLever = !rightLever;
+				}
+				
+				if (peopleCounted == 10)
+				{
+					buttonPressed = true;
+					tmr_interval.Enabled = false;
+					lbl_buttonFinish.Text = "Home: Yes!";
+					lbl_buttonFinish.ForeColor = Color.Green;
+				}
+			}
+			else
+			{
+				if (!leftLever && !personIsSaved[robotPosition - 1])
+				{
+					personIsSaved[robotPosition - 1] = true;
+					peopleSaved++;
+					leftLever = true;
+					
+					labelName = "lbl_d" + robotPosition + "Saved";
+					this.Controls[groupBoxName].Controls[labelName].Text = "Saved: Yes";
+					this.Controls[groupBoxName].Controls[labelName].ForeColor = Color.Green;
+					
+					labelName = "lbl_d" + robotPosition + "TurnSaved";
+					this.Controls[groupBoxName].Controls[labelName].Text = "On Turn: " + turnsTaken;
+				}
+				else
+				{
+					rightLever = !rightLever;
+				}
+			}
+			
+			tbr_leftLever.Value = Convert.ToInt32(leftLever);
+			tbr_rightLever.Value = Convert.ToInt32(rightLever);
+			
+			lbl_peopleSaved.Text = "People Saved: " + peopleSaved.ToString();
+			lbl_peopleCounted.Text = "People Counted: " + peopleCounted.ToString();
+			lbl_turns.Text = "Turns Taken: " + turnsTaken.ToString();
+			
+			labelName = "lbl_d" + robotPosition + "TimesVisited";
+			this.Controls[groupBoxName].Controls[labelName].Text = "Times Visited: " + timesVisited[robotPosition - 1];
+			
+			labelName = "lbl_d" + robotPosition + "Robot";
+			this.Controls[groupBoxName].Controls[labelName].Visible = true;
+			
+			this.Refresh();
+		}
+		
+		void Cbb_delaySelectedIndexChanged(object sender, EventArgs e)
+		{
+			delay = 1000 * float.Parse(cbb_delay.SelectedItem.ToString());
+			if (delay == 0)
+			{
+				tmr_interval.Interval = 1;
+			}
+			else
+			{
+				tmr_interval.Interval = (int)delay;
 			}
 		}
 	}
